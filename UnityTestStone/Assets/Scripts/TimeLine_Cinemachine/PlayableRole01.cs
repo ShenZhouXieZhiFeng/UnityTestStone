13b3fddb8c2e4e1a60c8d199f454868c93092a09
xiezhifeng <827443469@qq.com>
2021-09-21 16:09:04 +0800

    timeline

---
+++ UnityTestStone/Assets/Scripts/TimeLine_Cinemachine/PlayableRole01.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
+
+// A behaviour that is attached to a playable
+public class PlayableRole01 : PlayableBehaviour
+{
+
+    [Header("Dialog")]
+    public ExposedReference<Text> dialog;
+    public Text txtDialog;
+    [Multiline(3)]
+    public string dialogStr;
+
+
+    // Called when the owning graph starts playing
+    public override void OnGraphStart(Playable playable)
+    {
+
+    }
+
+    // Called when the owning graph stops playing
+    public override void OnGraphStop(Playable playable)
+    {
+        
+    }
+
+    // Called when the state of the playable is set to Play
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        txtDialog.gameObject.SetActive(true);
+        txtDialog.text = dialogStr;
+    }
+
+    // Called when the state of the playable is set to Paused
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        if (txtDialog)
+        {
+            txtDialog.gameObject.SetActive(false);
+        }
+    }
+
+    // Called each frame while the state is set to Play
+    public override void PrepareFrame(Playable playable, FrameData info)
+    {
+        
+    }
+}
---
+++ UnityTestStone/Assets/Scripts/TimeLine_Cinemachine/RolePlayableAsset01.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class RolePlayableAsset01 : PlayableAsset
+{
+    public ExposedReference<Text> talkText;
+    public string talkStr;
+
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        PlayableRole01 rolePlayable = new PlayableRole01();
+        rolePlayable.txtDialog = talkText.Resolve(graph.GetResolver());
+        rolePlayable.dialogStr = talkStr;
+        return ScriptPlayable<PlayableRole01>.Create(graph, rolePlayable);
+    }
+}
